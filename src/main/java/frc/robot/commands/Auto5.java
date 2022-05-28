3f8a59d1d8d6cf9b59d7c38a7a325a493614c479
jtso914 <josephtso914@gmail.com>
2022-05-28 19:28:52 -0400

    Update Auto5.java

--- src/main/java/frc/robot/commands/Auto5.java
+++ src/main/java/frc/robot/commands/Auto5.java
@@ -1,4 +1,5 @@
 package frc.robot.commands;
+
 import java.lang.invoke.ConstantCallSite;
 
 import edu.wpi.first.networktables.NetworkTable;
@@ -24,7 +25,7 @@ public class Auto5 extends CommandBase {
     private int step;
     private double startTime;
 
-    public Auto5(Limelight a, Hood h, InnerIndex i, Intake in, OuterIndex o, Shooter s, SwerveDrive swerve){
+    public Auto5(Limelight a, Hood h, InnerIndex i, Intake in, OuterIndex o, Shooter s, SwerveDrive swerve) {
         autoaim = a;
         hood = h;
         innerIndex = i;
@@ -44,138 +45,113 @@ public class Auto5 extends CommandBase {
     }
 
     @Override
-    public void initialize(){
+    public void initialize() {
         hood.hoodReset();
         intake.extend();
         swerveDrive.gyro.reset();
     }
 
     @Override
-    public void execute(){
-        
+    public void execute() {
+
+        // fix this
         NetworkTableInstance.getDefault().getTable("/datatable").getEntry("Auto2").setBoolean(true);
 
-
-        switch(step){
+        switch (step) {
             case 0:
-                if (!Constants.intakeStatus){
-                    intake.extend();
-                    Constants.intakeStatus = true;
-                }
-               
-                intake.spinIntake();
-                outerIndex.spin();
-
-                if (Math.abs(swerveDrive.getDriveDistance()) > 100 ){
-                    swerveDrive.stopAll();
-                    intake.retract();
-                    Constants.intakeStatus = false;
-                    step = 1;
-                    System.out.println("Drove 4 feet");
-                } else{
-                    swerveDrive.updatePeriodic(-0.014, 0.4, 0);
-                    
-                }
-        
-            break;
-
-            case 1:
-            //134
-                if (Math.abs(swerveDrive.getGyroAngle()) < 151 ){
-                    swerveDrive.updatePeriodic(0, 0, 0.15);
-                } else{
-                    intake.stopIntake();
-                    swerveDrive.stopAll();
-                    outerIndex.stop();
-                    step = 2;
-                }   
-            break;
-
-            case 2:   
                 hood.setHoodAngle(33);
                 shooter.set(3.67);
-                if(shooter.getRPM() > 1800) {
+                if (shooter.getRPM() > 1800) {
                     startTime = System.currentTimeMillis();
                     outerIndex.spin();
                     innerIndex.spin();
                     swerveDrive.resetDrive();
-                    step = 3;
-                }        
-            break;
-
-         
-            case 3:
-                    //swerveDrive.updatePeriodic(0, 0, 0);
-                    while (Math.abs(startTime - System.currentTimeMillis()) < 1500){
-                        continue;
-                    }
-                    shooter.set(0);
-                    outerIndex.stop();
-                    innerIndex.stop();
-                    step = 9;
-               
-            break;
-
-            case 4:
-                if (Math.abs(swerveDrive.getDriveDistance()) < 5){
-                    swerveDrive.updatePeriodic(0, 0.08, 0);
-                } else{
-                    swerveDrive.stopAll();
-                    hood.setHoodAngle(29);
-                    shooter.set(5);
-                    if(shooter.getRPM() > 2100) {
-                        outerIndex.spin();
-                        innerIndex.spin();
-                        shooter.set(0);
-                        step = 5;
-                    }
+                    step = 1;
                 }
-            break;
+                System.out.println("step " + step + " done");
+                break;
 
+            case 1:
+                while (Math.abs(startTime - System.currentTimeMillis()) < 1500) {
+                    continue;
+                }
+                shooter.set(0);
+                outerIndex.stop();
+                innerIndex.stop();
+                step = 2;
+                System.out.println("step " + step + " done");
+
+                break;
+            case 2:
+                if (Math.abs(swerveDrive.getGyroAngle()) < 34) {
+                    swerveDrive.updatePeriodic(0, 0, 0.15);
+                } else {
+                    swerveDrive.stopAll();
+                    swerveDrive.resetDrive();
+                    step = 3;
+                }
+                System.out.println("step " + step + " done");
+
+                break;
+            case 3:
+                if (!Constants.intakeStatus) {
+                    intake.extend();
+                    Constants.intakeStatus = true;
+                }
+
+                intake.spinIntake();
+                outerIndex.spin();
+
+                if (Math.abs(swerveDrive.getDriveDistance()) > 100) {
+                    swerveDrive.stopAll();
+                    intake.retract();
+                    Constants.intakeStatus = false;
+                    step = 4;
+                } else {
+                    swerveDrive.updatePeriodic(-0.014, 0.4, 0);
+
+                }
+
+                System.out.println("step " + step + " done");
+                break;
+            case 4:
+                if (Math.abs(swerveDrive.getGyroAngle()) < 70) {
+                    swerveDrive.updatePeriodic(0, 0, 0.15);
+                } else {
+                    swerveDrive.stopAll();
+                    step = 5;
+                }
+                System.out.println("step " + step + " done");
+
+                break;
             case 5:
-                // outerIndex.stop();
-                intake.stopIntake();
-                // innerIndex.stop();
-                step = 9;
-            break;
+                hood.setHoodAngle(33);
+                shooter.set(3.67);
+                if (shooter.getRPM() > 1800) {
+                    startTime = System.currentTimeMillis();
+                    outerIndex.spin();
+                    innerIndex.spin();
+                    swerveDrive.resetDrive();
+                    step = 6;
+                }
+                System.out.println("step " + step + " done");
+                break;
 
-            //turn around again
-            case 6:
-                
-            break;
-
-            //autoaim again 
-
-            case 7:
-                
-            break;
-
-
-            //shoot again
-
-            case 8:
-
-            break;
-
-            default:
-            step = 9;
-            break;
         }
-    }
-
-    @Override
-    public void end(boolean interrupted){
-    NetworkTableInstance.getDefault().getTable("/datatable").getEntry("Auto2").setBoolean(false);
-    shooter.set(0);
 
     }
 
     @Override
-    public boolean isFinished(){
-        //return (step >= 9);
+    public void end(boolean interrupted) {
+        NetworkTableInstance.getDefault().getTable("/datatable").getEntry("Auto2").setBoolean(false);
+        shooter.set(0);
+
+    }
+
+    @Override
+    public boolean isFinished() {
+        // return (step >= 9);
         return step >= 10;
     }
 
-    
-    
 }
